7f887594e169a966134165c4d3f8d1056fbb8df8
saitonaoki <pdcaword@gmail.com>
2017-06-13 17:46:40 +0900

    Unityデータ

---
+++ Geek-Unity-Project/Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+        private float speed = 13.0f;    //Ball速度調整
+
+        // Use this for initialization
+        void Start()
+        {
+
+            this.GetComponent<Rigidbody>().AddForce(    //RigidbodyはUnity側で実装確認
+                (transform.forward + transform.right) * speed,
+                ForceMode.VelocityChange);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
---
+++ Geek-Unity-Project/Assets/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定
+        if (collision.gameObject.tag == "Ball")
+        {
+            //相手のタグがBallならば、自分を消す
+            //Unity側でBall設定確認
+            Destroy(this.gameObject);
+        }
+    }
+}
